4da3bab2606d002234c0ef643596383c656b86e0
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 09:45:09 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,10 +2,11 @@
 require "pry"
 require "yaml"
 
-def load_library(filepath)
+def load_library(file)
 # code goes here
-emoticons = YAML.load_file(filepath)
-hash = {
+
+list = YAML.load_file(file)
+new_hash = {
   "get_meaning" => {},
   "get_emoticon" => {}
 }
@@ -18,24 +19,24 @@ end
 hash
 end
 
-def get_japanese_emoticon
-# code goes here
-def get_japanese_emoticon(filepath, emoji)
-hash = load_library(filepath)
-if hash["get_emoticon"][emoji] == nil
-  "Sorry, that emoticon was not found"
-else
-  hash["get_emoticon"][emoji]
-end
-end
-def get_english_meaning
-# code goes here
-end 
-def get_english_meaning(filepath, emoji)
-hash = load_library(filepath)
-if hash["get_meaning"][emoji] == nil
-  "Sorry, that emoticon was not found"
-else
-  hash["get_meaning"][emoji]
-end
-end
+# def get_japanese_emoticon
+# # code goes here
+# def get_japanese_emoticon(filepath, emoji)
+# hash = load_library(filepath)
+# if hash["get_emoticon"][emoji] == nil
+#   "Sorry, that emoticon was not found"
+# else
+#   hash["get_emoticon"][emoji]
+# end
+# end
+# def get_english_meaning
+# # code goes here
+# end 
+# def get_english_meaning(filepath, emoji)
+# hash = load_library(filepath)
+# if hash["get_meaning"][emoji] == nil
+#   "Sorry, that emoticon was not found"
+# else
+#   hash["get_meaning"][emoji]
+# end
+# end
